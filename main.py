19a0ba3a92994bc1371dde9e871e0f0f7fb93859
Happke <alehappke@gmail.com>
2025-04-08 13:06:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -63,4 +63,6 @@ def agendar():
         return jsonify({"status": "erro", "mensagem": resposta.text}), 500
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
